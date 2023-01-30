ad3541052e1d4d503bd1598caab9da5e97ab69ed
Morgan <107490669+Morganlth@users.noreply.github.com>
2023-01-30 12:24:37 +0100

    Add files via upload

---
+++ FrontEnd/assets/js/app.js
@@ -0,0 +1,13 @@
+//IMPORTS
+
+import { filter } from './modules/filter.js'
+import { login } from './modules/login.js'
+
+//CODE
+
+/*
+? Point d'entrée => set de filter.js & login.js
+*/
+
+filter.set()
+login.set()
